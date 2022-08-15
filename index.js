f7df150da711ffab2da9217e80ffef7c660f59a7
festus wanjohi <strategic.policyafrica@gmail.com>
2022-08-15 21:09:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride) {
   // Write your code here!
+  if (ride < 400) {
+    return "This one is on me!";
+  } else if (ride > 2500) {
+    return "No can do.";
+  } else if (ride > 2000) {
+    return "I will gladly take your thirty bucks.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  let ans;
+  city === "NYC" ? (ans = "Ok, sounds good.") : (ans = "No go.");
+  return ans;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
